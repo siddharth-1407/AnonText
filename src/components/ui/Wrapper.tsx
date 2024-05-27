6b28709a92be631aa0133635727ce59d812aa93a
siddharth-1407 <sanksiddharth@gmail.com>
2024-05-27 19:00:46 +0530

    fixed wrapper component for responisveness

--- src/components/ui/Wrapper.tsx
+++ src/components/ui/Wrapper.tsx
@@ -6,7 +6,11 @@ interface WrapperProps extends React.HTMLProps<HTMLDivElement> {
 }
 export default function Wrapper({ children, className }: WrapperProps) {
 	return (
-		<div className={cn('flex-1 flex flex-col min-h-screen container max-w-screen-md lg:max-w-screen-lg 2xl:max-w-screen-xl mx-auto', className)}>
+		<div
+			className={cn(
+				'flex-1 flex flex-col min-h-screen sm:container max-w-screen-md lg:max-w-screen-lg 2xl:max-w-screen-xl mx-auto',
+				className
+			)}>
 			{children}
 		</div>
 	);
